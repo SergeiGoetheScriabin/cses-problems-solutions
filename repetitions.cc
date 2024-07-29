78bc65f7a258f0151895906af9475565af7a8b46
SergeiGoetheScriabin <forliano@uiowa.edu>
2024-07-29 16:42:51 -0500

    solved repetitions

---
+++ repetitions.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{	
+	string s;
+	cin >> s;
+	int ans = 1, count = 1;
+	for (int i = 1; i < s.length(); i++) {
+		if(s[i] == s[i -1])
+			count += 1;
+		else
+			count = 1;
+		ans = max(ans, count);
+	}
+	cout <<  ans;
+}
+
+
